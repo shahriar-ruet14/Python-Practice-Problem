7a41ce8495b7c0d2a657e8291b649f429ffa10f0
Shahriar Mahmud <43475869+shahriarxbox@users.noreply.github.com>
2021-02-13 14:43:07 +0600

    1

--- Shafayets Plannet/sliding minimum window.py
+++ Shafayets Plannet/sliding minimum window.py
@@ -1,8 +1,13 @@
+import numpy as np
 def brute_rmq(arr,m):
 	res=[]
 	for i in range(0,len(arr)-m+1):
 		subarr=arr[i:i+m] #take subarray of size m, starting from index i
 		res.append(min(subarr)) #append the minimum element in result
 	return res
-print("This is atest")
+if __name__ == "__main__":
+	arr  = np.random.randint(100,size = 30)
+	print(len(arr))
+	print(len(brute_rmq(arr,3)))
+	pass
 
